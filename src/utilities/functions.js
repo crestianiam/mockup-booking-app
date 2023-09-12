b266a9ee7e307bc4ed8121f2d31bc9585e02327b
andrian <andrian.crestian95@gmail.com>
2023-09-12 12:13:39 +0200

    cleaning comments

--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -1,11 +1,11 @@
 export const getTodayDate = () => {
     const today = new Date()
-    return today.toISOString().slice(0, 10) // Formato: "YYYY-MM-DD"
+    return today.toISOString().slice(0, 10) // "YYYY-MM-DD"
 }
 
 export const getTomorrowDate = () => {
     const today = new Date()
     const tomorrow = new Date(today)
     tomorrow.setDate(tomorrow.getDate() + 1)
-    return tomorrow.toISOString().slice(0, 10) // Formato: "YYYY-MM-DD"
+    return tomorrow.toISOString().slice(0, 10) // "YYYY-MM-DD"
 }
